04ea93f0ea6c9f45fc6c1715813e7dc0f9d79552
n3-tsuguchi <tsuguchi@nnn.or.jp>
2024-02-29 17:00:03 +0900

    コミット

--- serverspec/spec/43.207.150.3/sample_spec.rb
+++ serverspec/spec/43.207.150.3/sample_spec.rb
@@ -1,10 +1,10 @@
 require 'spec_helper'
 
 describe package('git') do
-  it { should exist }
+  it { should be_installed }
 end
 
 describe package('nginx') do
-  it { should exist }
+  it { should be_installed }
 end
 
