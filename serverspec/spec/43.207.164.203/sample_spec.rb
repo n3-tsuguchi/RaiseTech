2650cdfcbee6a2e1772e4d0487075773724bba5b
n3-tsuguchi <tsuguchi@nnn.or.jp>
2024-04-03 14:31:11 +0900

    コミット

--- serverspec/spec/43.207.164.203/sample_spec.rb
+++ serverspec/spec/43.207.164.203/sample_spec.rb
@@ -1,5 +1,9 @@
 require 'spec_helper'
 
+set :backend, :ssh
+set :host, '43.207.164.203'
+set :ssh_options, :user => 'ec2-user', :keys => ['kawakami.pem']
+
 describe package('git') do
   it { should be_installed }
 end
@@ -10,7 +14,7 @@ end
 
 describe 'SSH connection' do
   it 'should connect to the remote host' do
-    Net::SSH.start('54.249.215.37', 'ec2-user', keys: ['kawakami.pem']) do |ssh|
+    Net::SSH.start('43.207.164.203', 'ec2-user', keys: ['kawakami.pem']) do |ssh|
       # ここにSSH接続が成功した場合のテストコードを記述する
       # 例えば、特定のコマンドを実行して結果を検証するなど
     end
