6d2b1fc6c73947d91d5777898aecfe1b08a09adc
n3-tsuguchi <tsuguchi@nnn.or.jp>
2024-04-03 14:27:04 +0900

    IPアドレスを変更

--- serverspec/spec/54.249.215.37/sample_spec.rb
+++ serverspec/spec/54.249.215.37/sample_spec.rb
@@ -10,7 +10,7 @@ end
 
 describe 'SSH connection' do
   it 'should connect to the remote host' do
-    Net::SSH.start('43.207.83.40', 'ec2-user', keys: ['kawakami.pem']) do |ssh|
+    Net::SSH.start('54.249.215.37', 'ec2-user', keys: ['kawakami.pem']) do |ssh|
       # ここにSSH接続が成功した場合のテストコードを記述する
       # 例えば、特定のコマンドを実行して結果を検証するなど
     end
